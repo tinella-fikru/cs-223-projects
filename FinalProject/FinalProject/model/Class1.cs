d9b5b960a00fe32c354acf81e163fccb30375c90
tinella <tinellafikru28@gmail.com>
2022-08-18 18:36:52 +0300

    start

---
+++ FinalProject/FinalProject/model/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject.model
+{
+    internal class Class1
+    {
+        static private List<Class1> class1 = new List<Class1>();
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string confirmPassword { get; set; }
+
+        public int phone { get; set; }
+
+
+
+
+        public void save()
+        {
+            class1.Add(this);
+            MessageBox.Show("Successfully Signed In!!!");
+        }
+        public static Class1 findOne(string name)
+        {
+            return class1.Find(c => c.Name == name);
+        }
+
+        public static List<Class1> getAllProducts()
+        {
+            return class1;
+        }
+    }
+}
+
